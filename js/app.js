6395bf14a063e508d124f945f6b1d15999b49343
sowad33 <sowadkhan33@gmail.com>
2022-03-09 18:35:19 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,26 @@
+const API_KEY = `3378fadaaa2d9e92cfceba054a780dce`;
+const searchTemperature = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    // console.log(url);
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayTemperature(data));
+}
+
+const setInnerText = (id,text) => {
+    document.getElementById(id).innerText = text;
+}
+
+const displayTemperature = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('condition', temperature.weather[0].main);
+    // console.log(temperature);
+
+    // set weather icon
+    const url = ` http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', url);
+}
